4416525f6bd17eb976a448253fe97cf9807bfe55
Manas Purohit <purohit.m@husky.neu.edu>
2017-07-05 18:25:41 +0200

    fixed empty queue bug

--- app/src/main/java/hu/ait/youtubequeuer/adapter/QueueRecyclerAdapter.java
+++ app/src/main/java/hu/ait/youtubequeuer/adapter/QueueRecyclerAdapter.java
@@ -1,7 +1,6 @@
 package hu.ait.youtubequeuer.adapter;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +11,6 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import hu.ait.youtubequeuer.R;
