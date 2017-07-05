df0b7905379e1dfdda402f17674a60999e52ae3d
Manas Purohit <purohit.m@husky.neu.edu>
2017-07-05 18:38:14 +0200

    fixed removing first item bug

--- app/src/main/java/hu/ait/youtubequeuer/adapter/QueueRecyclerAdapter.java
+++ app/src/main/java/hu/ait/youtubequeuer/adapter/QueueRecyclerAdapter.java
@@ -1,7 +1,9 @@
 package hu.ait.youtubequeuer.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import hu.ait.youtubequeuer.R;
@@ -62,6 +65,11 @@ public class QueueRecyclerAdapter extends RecyclerView.Adapter<QueueRecyclerAdap
 
     @Override
     public void onItemDismiss(int position) {
+        if (position == 0) {
+            notifyDataSetChanged();
+            return;
+        }
+
         realmVideoQueue.beginTransaction();
         videoQueue.get(position).deleteFromRealm();
         realmVideoQueue.commitTransaction();
